fcd8fa0a0011f0612ec68bf72e4e801247f4e7fe
sammykeyes <ayushisan@gmail.com>
2015-08-14 13:42:00 +0000

    logo, ruby, css

--- models/model.rb
+++ models/model.rb
@@ -1,2 +1,11 @@
+class Mood
+  attr_accessor :name, :color , :age
+  def initialize (name, breed, age)
+    @name = name.capitalize
+    @breed = breed.capitalize
+    @age = age
+  end 
+  
+end 
 "Hello #{name}, 
 Since you are feeling #{mood}, here are some suggestions for you to explore."
